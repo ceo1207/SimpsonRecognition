db854078d9964f640cb95f9b3a3023707a03ca31
ceo1207 <water799@126.com>
2018-07-29 09:21:17 +0800

    do some changes

--- train.py
+++ train.py
@@ -18,10 +18,14 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD, Adam
 
-map_characters = {0: 'abraham_grampa_simpson', 1: 'apu_nahasapeemapetilon', 2: 'bart_simpson', 
-        3: 'charles_montgomery_burns', 4: 'chief_wiggum', 5: 'comic_book_guy', 6: 'edna_krabappel', 
-        7: 'homer_simpson', 8: 'kent_brockman', 9: 'krusty_the_clown', 10: 'lisa_simpson', 
-        11: 'marge_simpson', 12: 'milhouse_van_houten', 13: 'moe_szyslak', 
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+map_characters = {0: 'abraham_grampa_simpson', 1: 'apu_nahasapeemapetilon', 2: 'bart_simpson',
+        3: 'charles_montgomery_burns', 4: 'chief_wiggum', 5: 'comic_book_guy', 6: 'edna_krabappel',
+        7: 'homer_simpson', 8: 'kent_brockman', 9: 'krusty_the_clown', 10: 'lisa_simpson',
+        11: 'marge_simpson', 12: 'milhouse_van_houten', 13: 'moe_szyslak',
         14: 'ned_flanders', 15: 'nelson_muntz', 16: 'principal_skinner', 17: 'sideshow_bob'}
 
 pic_size = 64
@@ -31,9 +35,10 @@ num_classes = len(map_characters)
 pictures_per_class = 1000
 test_size = 0.15
 
+
 def load_pictures(BGR):
     """
-    Load pictures from folders for characters from the map_characters dict and create a numpy dataset and 
+    Load pictures from folders for characters from the map_characters dict and create a numpy dataset and
     a numpy labels set. Pictures are re-sized into picture_size square.
     :param BGR: boolean to use true color for the picture (RGB instead of BGR for plt)
     :return: dataset, labels set
@@ -41,67 +46,72 @@ def load_pictures(BGR):
     pics = []
     labels = []
     for k, char in map_characters.items():
-        pictures = [k for k in glob.glob('./characters/%s/*' % char)]
-        nb_pic = round(pictures_per_class/(1-test_size)) if round(pictures_per_class/(1-test_size))<len(pictures) else len(pictures)
+        pictures = glob.glob('/home/ceo1207/PycharmProjects/Datasets/characters/%s/*' % char)
+        nb_pic = int(round(pictures_per_class/(1-test_size))) if round(pictures_per_class/(1-test_size))<len(pictures) else len(pictures)
         # nb_pic = len(pictures)
-        for pic in np.random.choice(pictures, nb_pic):
+        pic_list = np.random.choice(pictures, nb_pic)
+        for pic in pic_list:
             a = cv2.imread(pic)
-            if BGR:
-                a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
-            a = cv2.resize(a, (pic_size,pic_size))
-            pics.append(a)
-            labels.append(k)
-    return np.array(pics), np.array(labels) 
+            if a is not None:
+                if BGR:
+                    a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+                a = cv2.resize(a, (pic_size,pic_size))
+                pics.append(a)
+                labels.append(k)
+    return np.array(pics), np.array(labels)
 
-def get_dataset(save=False, load=False, BGR=False):
+
+def get_dataset(save=True, load=False, BGR=False):
     """
     Create the actual dataset split into train and test, pictures content is as float32 and
     normalized (/255.). The dataset could be saved or loaded from h5 files.
     :param save: saving or not the created dataset
     :param load: loading or not the dataset
     :param BGR: boolean to use true color for the picture (RGB instead of BGR for plt)
-    :return: X_train, X_test, y_train, y_test (numpy arrays)
+    :return: x_train, x_test, y_train, y_test (numpy arrays)
     """
     if load:
         h5f = h5py.File('dataset.h5','r')
-        X_train = h5f['X_train'][:]
-        X_test = h5f['X_test'][:]
-        h5f.close()    
+        x_train = h5f['x_train'][:]
+        x_test = h5f['x_test'][:]
+        h5f.close()
 
         h5f = h5py.File('labels.h5','r')
         y_train = h5f['y_train'][:]
         y_test = h5f['y_test'][:]
-        h5f.close()    
+        h5f.close()
     else:
         X, y = load_pictures(BGR)
         y = keras.utils.to_categorical(y, num_classes)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+        x_train = x_train.astype('float32') / 255.
+        x_test = x_test.astype('float32') / 255.
+        print("Train", x_train.shape, y_train.shape)
+        print("Test", x_test.shape, y_test.shape)
+
         if save:
             h5f = h5py.File('dataset.h5', 'w')
-            h5f.create_dataset('X_train', data=X_train)
-            h5f.create_dataset('X_test', data=X_test)
+            h5f.create_dataset('x_train', data=x_train)
+            h5f.create_dataset('x_test', data=x_test)
             h5f.close()
 
             h5f = h5py.File('labels.h5', 'w')
             h5f.create_dataset('y_train', data=y_train)
             h5f.create_dataset('y_test', data=y_test)
             h5f.close()
-            
-    X_train = X_train.astype('float32') / 255.
-    X_test = X_test.astype('float32') / 255.
-    print("Train", X_train.shape, y_train.shape)
-    print("Test", X_test.shape, y_test.shape)
+
+
     if not load:
-        dist = {k:tuple(d[k] for d in [dict(Counter(np.where(y_train==1)[1])), dict(Counter(np.where(y_test==1)[1]))]) 
-                for k in range(num_classes)}
-        print('\n'.join(["%s : %d train pictures & %d test pictures" % (map_characters[k], v[0], v[1]) 
+        dist = {k:tuple(d[k] for d in [dict(Counter(np.where(y_train==1)[1])), dict(Counter(np.where(y_test==1)[1]))])
+            for k in range(num_classes)}
+        print('\n'.join(["%s : %d train pictures & %d test pictures" % (map_characters[k], v[0], v[1])
             for k,v in sorted(dist.items(), key=lambda x:x[1][0], reverse=True)]))
-    return X_train, X_test, y_train, y_test
+    return x_train, x_test, y_train, y_test
 
 def create_model_four_conv(input_shape):
     """
     CNN Keras model with 4 convolutions.
-    :param input_shape: input shape, generally X_train.shape[1:]
+    :param input_shape: input shape, generally x_train.shape[1:]
     :return: Keras model, RMS prop optimizer
     """
     model = Sequential()
@@ -131,7 +141,7 @@ def create_model_four_conv(input_shape):
 def create_model_six_conv(input_shape):
     """
     CNN Keras model with 6 convolutions.
-    :param input_shape: input shape, generally X_train.shape[1:]
+    :param input_shape: input shape, generally x_train.shape[1:]
     :return: Keras model, RMS prop optimizer
     """
     model = Sequential()
@@ -149,7 +159,7 @@ def create_model_six_conv(input_shape):
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
-    model.add(Conv2D(256, (3, 3), padding='same')) 
+    model.add(Conv2D(256, (3, 3), padding='same'))
     model.add(Activation('relu'))
     model.add(Conv2D(256, (3, 3)))
     model.add(Activation('relu'))
@@ -180,7 +190,7 @@ def lr_schedule(epoch):
     lr = 0.01
     return lr*(0.1**int(epoch/10))
 
-def training(model, X_train, X_test, y_train, y_test, data_augmentation=True):
+def training(model, x_train, x_test, y_train, y_test, data_augmentation=True):
     """
     Training.
     :param model: Keras sequential model
@@ -203,29 +213,43 @@ def training(model, X_train, X_test, y_train, y_test, data_augmentation=True):
             vertical_flip=False)  # randomly flip images
         # Compute quantities required for feature-wise normalization
         # (std, mean, and principal components if ZCA whitening is applied).
-        datagen.fit(X_train)
-        filepath="weights_6conv_%s.hdf5" % time.strftime("%d%m/%Y")
+        datagen.fit(x_train)
+        filepath="checkpoint-{epoch:02d}-{val_acc:.2f}.hdf5"
         checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=0, save_best_only=True, mode='max')
         callbacks_list = [LearningRateScheduler(lr_schedule) ,checkpoint]
-        history = model.fit_generator(datagen.flow(X_train, y_train,
+        history = model.fit_generator(datagen.flow(x_train, y_train,
                                     batch_size=batch_size),
-                                    steps_per_epoch=X_train.shape[0] // batch_size,
-                                    epochs=40,
-                                    validation_data=(X_test, y_test),
-                                    callbacks=callbacks_list)        
+                                    steps_per_epoch=x_train.shape[0] // batch_size,
+                                    epochs=10,
+                                    validation_data=(x_test, y_test),
+                                    callbacks=callbacks_list)
     else:
-        history = model.fit(X_train, y_train,
+        history = model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
-          validation_data=(X_test, y_test),
+          validation_data=(x_test, y_test),
           shuffle=True)
     return model, history
 
 if __name__ == '__main__':
-    X_train, X_test, y_train, y_test = train.get_dataset(load=True)
-    model, opt = train.create_model_six_conv(X_train.shape[1:])
+    # get data from the directory containing characters images
+    # first time  use load=False, save=True
+    x_train, x_test, y_train, y_test = get_dataset(save=True, load=False)
+    # second time  use load=True, save=false
+    # x_train, x_test, y_train, y_test = get_dataset(save=False, load=True)
+    model, opt = create_model_six_conv(x_train.shape[1:])
     model.compile(loss='categorical_crossentropy',
               optimizer=opt,
               metrics=['accuracy'])
-    model, history = train.training(model, X_train, X_test, y_train, y_test, data_augmentation=True)
+    model, history = training(model, x_train, x_test, y_train, y_test, data_augmentation=True)
 
+'''
+    x_train, x_test, y_train, y_test = get_dataset(save=True, load=False)
+    # second time  use load=True, save=false
+    # x_train, x_test, y_train, y_test = get_dataset(save=False, load=True)
+    model, opt = train.create_model_six_conv(x_train.shape[1:])
+    model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+    model, history = train.training(model, x_train, x_test, y_train, y_test, data_augmentation=True)
+'''
